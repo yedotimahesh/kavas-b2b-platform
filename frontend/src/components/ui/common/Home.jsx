74ab7fd663e794fc9f69370cbeceaa5e4fd3de00
saikrishna2001 <chittetisai15@gmail.com>
2026-03-26 18:47:11 +0530

    hero

--- frontend/src/components/ui/common/Home.jsx
+++ frontend/src/components/ui/common/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Hero from './Hero'
 const Home = () => {
+    
+
   return (
-    <div>Home</div>
+    <>
+    <Hero/>
+    </>
   )
 }
 
--- frontend/src/components/ui/common/Navbar.jsx
+++ frontend/src/components/ui/common/Navbar.jsx
@@ -1,6 +1,5 @@
 "use client";
 import React from 'react'
-
 import { Button } from "@/components/ui/button";
 import { MapPin, Search, ShoppingCart, Moon, ArrowRight, Heart , ChevronDown} from "lucide-react";
 
